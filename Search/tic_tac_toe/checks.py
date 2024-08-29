ace8d32a18bf93b0658f4debe621e69350ca6fc2
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-08-29 13:15:33 +0300

    Save Code

--- Search/tic_tac_toe/checks.py
+++ Search/tic_tac_toe/checks.py
@@ -1,1 +1,11 @@
 # Check project related things
+
+# import tictactoe
+#
+# current_state = [[tictactoe.EMPTY, "X", tictactoe.EMPTY],
+#                  [tictactoe.EMPTY, tictactoe.EMPTY, tictactoe.EMPTY],
+#                  ["X", "O", tictactoe.EMPTY]]
+#
+# print(tictactoe.player(current_state))
+
+
