05dde7c15f32544a39d2adf9de1ccc479aa8d16a
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-08-29 16:19:01 +0300

    Save Code

--- Search/tic_tac_toe/checks.py
+++ Search/tic_tac_toe/checks.py
@@ -18,12 +18,21 @@
 
 # print(tictactoe.actions(current_state))
 
+# import tictactoe
+#
+# current_state = [[tictactoe.EMPTY, "X", tictactoe.EMPTY],
+#                  [tictactoe.EMPTY, tictactoe.EMPTY, tictactoe.EMPTY],
+#                  ["X", "O", tictactoe.EMPTY]]
+#
+# print(tictactoe.result(current_state, (0, 0)))
+# current_state = tictactoe.result(current_state, (0, 0))
+# print(tictactoe.result(current_state, (0, 2)))
+
+# --
 import tictactoe
 
 current_state = [[tictactoe.EMPTY, "X", tictactoe.EMPTY],
                  [tictactoe.EMPTY, tictactoe.EMPTY, tictactoe.EMPTY],
                  ["X", "O", tictactoe.EMPTY]]
 
-print(tictactoe.result(current_state, (0, 0)))
-current_state = tictactoe.result(current_state, (0, 0))
-print(tictactoe.result(current_state, (0, 2)))
+
