d2fa119b9706f6f20d0eb03fe5ec8cd4e4191a50
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-08-29 13:05:24 +0300

    Save Code

--- Search/tic_tac_toe/tictactoe.py
+++ Search/tic_tac_toe/tictactoe.py
@@ -13,3 +13,14 @@ def initial_state():
     return [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    x_moves = sum([m.count("X") for m in board])
+    o_moves = sum([m.count("O") for m in board])
+
+    return O if x_moves > o_moves else X
+
