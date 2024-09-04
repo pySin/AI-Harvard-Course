d7a8512fcbf2e2734dbba6720298e0965fae30f2
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-09-04 12:09:40 +0300

    Save Code

--- Search/tic_tac_toe/tictactoe_test.py
+++ Search/tic_tac_toe/tictactoe_test.py
@@ -17,3 +17,9 @@ def play_ai_vs_ai():
     board = ttt.initial_state()
     game_over = False
 
+    while not game_over:
+        move = ttt.minimax(board)
+        board = ttt.result(board, move)
+        game_over = ttt.terminal(board)
+
+    assert ttt.winner(board) is None
