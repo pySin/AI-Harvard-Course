bedab48883338a68e8c70ca7ac8eb7ab55e86ae0
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-09-04 12:03:26 +0300

    Save Code

--- Search/tic_tac_toe/tictactoe_test.py
+++ Search/tic_tac_toe/tictactoe_test.py
@@ -2,3 +2,9 @@
 import pytest as pt
 
 import tictactoe as ttt
+
+# The AI plays against itself
+
+@pt.mark.parametrize("execution_number", range(10))
+def test(execution_number):
+    return play_ai_vs_ai()
