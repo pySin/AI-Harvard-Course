e9d8ea0c38b9b57e5243719514e3d3818803c73d
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-09-05 17:13:35 +0300

    Save Code

--- Search/tic_tac_toe/checks.py
+++ Search/tic_tac_toe/checks.py
@@ -195,11 +195,16 @@ def generate_number():
     return random.randint(1, 9)
 
 
+def modify_number(num):
+    modifier = random.randint(-1, 1)
+    return num + modifier
+
+
 def number_1(num_set):
     if terminal(num_set):
         return f"The last three digits equals to {sum(num_set[-3:])}"
 
-    new_number = None
+    new_number = modify_number(generate_number())
 
 
 def number_2(num_set):
