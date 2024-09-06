40d4f2c556d49dc668f0040db8d38f821b5bb96b
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-09-06 15:33:40 +0300

    Save Code

--- knowledge/knights/puzzle.py
+++ knowledge/knights/puzzle.py
@@ -1,2 +1,11 @@
 # Puzzles for the Knights challenge
 from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
