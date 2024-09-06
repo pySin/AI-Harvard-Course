ccfc1211840952f4bc54e38715fc1058e7a6e1fd
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-09-06 14:05:59 +0300

    Save Code

--- knowledge/knights/logic.py
+++ knowledge/knights/logic.py
@@ -1,1 +1,17 @@
 import itertools
+
+
+class Sentence():
+
+    def evaluate(self, model):
+        """Evaluates the logical sentence."""
+        raise Exception("nothing to evaluate")
+
+    def formula(self):
+        """Returns string formula representing logical sentence."""
+        return ""
+
+    def symbols(self):
+        """Returns a set of all symbols in the logical sentence."""
+        return set()
+
