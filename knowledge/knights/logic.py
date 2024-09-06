8542e4b675181123c4f501b09812bf9cf93a51cb
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-09-06 14:12:27 +0300

    Save Code

--- knowledge/knights/logic.py
+++ knowledge/knights/logic.py
@@ -47,3 +47,16 @@ class Symbol(Sentence):
 
     def __init__(self, name):
         self.name = name
+
+    def __eq__(self, other):
+        return isinstance(other, Symbol) and self.name == other.name
+
+    def __hash__(self):
+        return hash(("symbol", self.name))
+
+    def __repr__(self):
+        return self.name
+
+
+
+
