dbd283c7b3bf63e0bdae6e664fded977b377b1c1
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-09-06 14:17:29 +0300

    Save Code

--- knowledge/knights/logic.py
+++ knowledge/knights/logic.py
@@ -81,5 +81,10 @@ class Not(Sentence):
     def __eq__(self, other):
         return isinstance(other, Not) and self.operand == other.operand
 
+    def __hash__(self):
+        return hash(("not", hash(self.operand)))
+
+    def __repr__(self):
+        return f"Not({self.operand})"
 
 
