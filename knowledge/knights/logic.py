14b6597dd06e4bc2f7eb3e1ef294e9fe2f730676
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-09-06 14:34:03 +0300

    Save Code

--- knowledge/knights/logic.py
+++ knowledge/knights/logic.py
@@ -147,3 +147,11 @@ class Or(Sentence):
         return hash(
             ("or", tuple(hash(disjunct) for disjunct in self.disjuncts))
         )
+
+    def __repr__(self):
+        disjuncts = ", ".join([str(disjunct) for disjunct in self.disjuncts])
+        return f"Or({disjuncts})"
+
+    def evaluate(self, model):
+        return any(disjunct.evaluate(model) for disjunct in self.disjuncts)
+
