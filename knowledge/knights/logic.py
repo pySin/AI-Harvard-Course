de2437d4e2408863fcdc92c1298192d3d237203b
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-09-06 14:31:00 +0300

    Save Code

--- knowledge/knights/logic.py
+++ knowledge/knights/logic.py
@@ -132,3 +132,18 @@ class And(Sentence):
 
     def symbols(self):
         return set.union(*[conjunct.symbols() for conjunct in self.conjuncts])
+
+
+class Or(Sentence):
+    def __init__(self, *disjuncts):
+        for disjunct in disjuncts:
+            Sentence.validate(disjunct)
+        self.disjuncts = list(disjuncts)
+
+    def __eq__(self, other):
+        return isinstance(other, Or) and self.disjuncts == other.disjuncts
+
+    def __hash__(self):
+        return hash(
+            ("or", tuple(hash(disjunct) for disjunct in self.disjuncts))
+        )
