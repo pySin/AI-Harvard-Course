5337e8de7a29f16c4c9fc13e98dea5f6f130fc12
Sinan Mehmedov <sinanat@abv.bg>
2024-09-25 00:27:14 +0100

    Save Code

--- Uncertainty/bayes_network/model.py
+++ Uncertainty/bayes_network/model.py
@@ -17,3 +17,20 @@ maintenance = Node(ConditionalProbabilityTable([
     ["heavy", "yes", 0.1],
     ["heavy", "no", 0.9]
 ], [rain.distribution]), name="maintenance")
+
+
+# Train node is conditional on rain and maintenance
+train = Node(ConditionalProbabilityTable([
+    ["none", "yes", "on time", 0.8],
+    ["none", "yes", "delayed", 0.2],
+    ["none", "no", "on time", 0.9],
+    ["none", "no", "delayed", 0.1],
+    ["light", "yes", "on time", 0.6],
+    ["light", "yes", "delayed", 0.4],
+    ["light", "no", "on time", 0.7],
+    ["light", "no", "delayed", 0.3],
+    ["heavy", "yes", "on time", 0.4],
+    ["heavy", "yes", "delayed", 0.6],
+    ["heavy", "no", "on time", 0.5],
+    ["heavy", "no", "delayed", 0.5],
+], [rain.distribution, maintenance.distribution]), name="train")
