15c0e30890af7057eb2fb06be3b58c5535946746
Sinan Mehmedov <sinanat@abv.bg>
2024-09-28 16:11:54 +0100

    Save Code

--- Uncertainty/chain/model.py
+++ Uncertainty/chain/model.py
@@ -5,3 +5,12 @@ start = DiscreteDistribution({
     "sun": 0.5,
     "rain": 0.5
 })
+
+
+# Define transition model
+transitions = ConditionalProbabilityTable([
+    ["sun", "sun", 0.8],
+    ["sun", "rain", 0.2],
+    ["rain", "sun", 0.3],
+    ["rain", "rain", 0.7]
+], [start])
