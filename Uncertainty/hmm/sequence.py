f3b43f9e4ad19baac56056eebd2d5226e206bc3f
Sinan Mehmedov <sinanat@abv.bg>
2024-09-30 11:33:35 +0100

    Save Code

---
+++ Uncertainty/hmm/sequence.py
@@ -0,0 +1,14 @@
+from model import model
+
+# Observed data
+observations = [
+    "umbrella",
+    "umbrella",
+    "no umbrella",
+    "umbrella",
+    "umbrella",
+    "umbrella",
+    "umbrella",
+    "no umbrella",
+    "no umbrella"
+]
