ad40cef87ab473853dd6efab923dfa2df8f13921
Sinan Mehmedov <sinanat@abv.bg>
2024-10-01 00:51:51 +0100

    Save Code

--- Uncertainty/PageRank/pagerank.py
+++ Uncertainty/PageRank/pagerank.py
@@ -58,3 +58,15 @@ def transition_model(corpus, page, damping_factor):
     a link at random chosen from all pages in the corpus.
     """
     raise NotImplementedError
+
+
+def sample_pagerank(corpus, damping_factor, n):
+    """
+    Return PageRank values for each page by sampling `n` pages
+    according to transition model, starting with a page at random.
+
+    Return a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1). All
+    PageRank values should sum to 1.
+    """
+    raise NotImplementedError
