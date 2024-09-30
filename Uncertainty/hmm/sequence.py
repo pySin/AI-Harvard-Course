be07af036b50eeecacb278acf932f8756092e3fe
Sinan Mehmedov <sinanat@abv.bg>
2024-09-30 11:35:23 +0100

    Save Code

--- Uncertainty/hmm/sequence.py
+++ Uncertainty/hmm/sequence.py
@@ -12,3 +12,8 @@ observations = [
     "no umbrella",
     "no umbrella"
 ]
+
+# Predict underlying states
+predictions = model.predict(observations)
+for prediction in predictions:
+    print(model.states[prediction].name)
