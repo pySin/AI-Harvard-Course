0d3733d504f7b5f59f0dbb08dd4cca40de93d40f
Sinan Mehmedov <sinanat@abv.bg>
2024-10-01 14:49:25 +0100

    Save Code

--- Uncertainty/PageRank/checks_pagerank.py
+++ Uncertainty/PageRank/checks_pagerank.py
@@ -19,6 +19,11 @@ page_1 = "1.html"
 def transition_model(corpus, page, damping_factor):
     links = corpus[page]
     print(links)
+    visit_probabilities = {x: round((1 - damping_factor) / len(corpus), 3) for x in corpus}
+    visit_probabilities = {x: visit_probabilities[x] + (damping_factor / len(links))
+                           if x in links else visit_probabilities[x]
+                           for x in visit_probabilities}
+    print(visit_probabilities)
 
 
 transition_model(corpus_1, page_1, DAMPING)
