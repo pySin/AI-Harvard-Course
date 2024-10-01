82eab1c1d8765e6ee883c9955063cd6eaad46a51
Sinan Mehmedov <sinanat@abv.bg>
2024-10-01 14:16:29 +0100

    Save Code

--- Uncertainty/PageRank/checks_pagerank.py
+++ Uncertainty/PageRank/checks_pagerank.py
@@ -5,3 +5,13 @@ import sys
 
 DAMPING = 0.85
 SAMPLES = 10000
+
+
+corpus = {
+    "1.html": {"2.html", "3.html"},
+    "2.html": {"3.html"},
+    "3.html": {"2.html"}
+}
+
+def transition_model(corpus, page, damping_factor):
+    pass
