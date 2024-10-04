84736a5f13727794f83242ea6fdd56acd4fc3e27
Sinan Mehmedov <sinanat@abv.bg>
2024-10-04 15:09:07 +0100

    Save Code

--- Uncertainty/PageRank/pagerank_test.py
+++ Uncertainty/PageRank/pagerank_test.py
@@ -1,4 +1,9 @@
 import random as rd
 import pytest as pt
 
+from pagerank import DAMPING, crawl, iterate_pagerank, sample_pagerank
+
+TOLERANCE = 1e-3  # Error tolerance = ±0.001 when comparing sample and iterate results
+SAMPLES = 10 ** 6  # More samples => better result
+
 
