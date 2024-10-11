3ab4c5b7b20e88db06f4716a3f970619d94818cf
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-10-11 13:10:12 +0200

    Save Code

--- side_things/SQL_file_import.py
+++ side_things/SQL_file_import.py
@@ -1,12 +1,17 @@
 # SQL file import
 import mysql.connector
 
-file_path = r"C:\Users\1\Desktop\Bioinformatics\Autism Spectrum Disorder RNA-seq\NCBI_gene_info_human"
+FILE_PATH = r"C:\Users\1\Desktop\Bioinformatics\Autism Spectrum Disorder RNA-seq\NCBI_gene_info_human"
 
-o_file = open(file_path, "r")
-lines = o_file.readlines()
-print(len(lines))
+
+def read_first_line(file_path):
+    o_file = open(file_path, "r")
+    first_line = o_file.readline()
+
+# lines = o_file.readlines()
+# print(len(lines))
+
 
 conn = mysql.connector.connect(host='localhost', user='root',
                                password='dance')  # MySQL connection.
-print(conn)
+cursor = conn.cursor()
