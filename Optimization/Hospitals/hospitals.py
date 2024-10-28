ed5f0c508aafe3b8f23965936c7e509d92ed967a
Sinan Mehmedov <sinanat@abv.bg>
2024-10-28 15:48:58 +0000

    Save Code

--- Optimization/Hospitals/hospitals.py
+++ Optimization/Hospitals/hospitals.py
@@ -31,3 +31,19 @@ class Space():
         for hospital in self.hospitals:
             candidates.remove(hospital)
         return candidates
+
+    def hill_climb(self, maximum=None, image_prefix=None, log=False):
+        """Performs hill-climbing to find a solution."""
+        count = 0
+
+        # Start by initializing hospitals randomly
+        self.hospitals = set()
+        for i in range(self.num_hospitals):
+            self.hospitals.add(random.choice(list(self.available_spaces())))
+        if log:
+            print("Initial state: cost", self.get_cost(self.hospitals))
+        if image_prefix:
+            self.output_image(f"{image_prefix}{str(count).zfill(3)}.png")
+
+
+
