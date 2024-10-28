a411543d7c15babe0b9ed0e411080c8735c7739d
Sinan Mehmedov <sinanat@abv.bg>
2024-10-28 15:35:40 +0000

    Save Code

---
+++ Optimization/Hospitals/hospitals.py
@@ -0,0 +1,14 @@
+import random
+
+
+class Space():
+
+    def __init__(self, height, width, num_hospitals):
+        """Create a new state space with given dimensions."""
+        self.height = height
+        self.width = width
+        self.num_hospitals = num_hospitals
+        self.houses = set()
+        self.hospitals = set()
+
+
