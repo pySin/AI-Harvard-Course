dd5142e470d758135adabb21130491a6f0ddf304
Sinan Mehmedov <sinanat@abv.bg>
2024-10-29 18:39:24 +0000

    Save Code

--- Optimization/Scheduling/schedule0.py
+++ Optimization/Scheduling/schedule0.py
@@ -13,3 +13,21 @@ CONSTRAINTS = [
     ("F", "G")
 ]
 
+
+def backtrack(assignment):
+    """Runs backtracking search to find an assignment."""
+
+    # Check if assignment is complete
+    if len(assignment) == len(VARIABLES):
+        return assignment
+
+    # Try a new variable
+    var = select_unassigned_variable(assignment)
+    for value in ["Monday", "Tuesday", "Wednesday"]:
+        new_assignment = assignment.copy()
+        new_assignment[var] = value
+        if consistent(new_assignment):
+            result = backtrack(new_assignment)
+            if result is not None:
+                return result
+    return None
