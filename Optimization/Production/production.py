d64853a90c90bcd9e82d5f0eaa93437321b58650
Sinan Mehmedov <sinanat@abv.bg>
2024-10-29 14:19:35 +0000

    Save Code

--- Optimization/Production/production.py
+++ Optimization/Production/production.py
@@ -4,4 +4,10 @@ import scipy.optimize
 # Constraint 1: 5x_1 + 2x_2 <= 20
 # Constraint 2: -10x_1 + -12x_2 <= -90
 
+result = scipy.optimize.linprog(
+    [50, 80],  # Cost function: 50x_1 + 80x_2
+    A_ub=[[5, 2], [-10, -12]],  # Coefficients for inequalities
+    b_ub=[20, -90],  # Constraints for inequalities: 20 and -90
+)
+
 
