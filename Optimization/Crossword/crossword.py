66a6af8d198110be4a85ecd50e002ec87fa33627
Sinan Mehmedov <sinanat@abv.bg>
2024-11-01 17:24:38 +0000

    Save Code

--- Optimization/Crossword/crossword.py
+++ Optimization/Crossword/crossword.py
@@ -3,3 +3,16 @@ class Variable():
     ACROSS = "across"
     DOWN = "down"
 
+    def __init__(self, i, j, direction, length):
+        """Create a new variable with starting point, direction, and length."""
+        self.i = i
+        self.j = j
+        self.direction = direction
+        self.length = length
+        self.cells = []
+        for k in range(self.length):
+            self.cells.append(
+                (self.i + (k if self.direction == Variable.DOWN else 0),
+                 self.j + (k if self.direction == Variable.ACROSS else 0))
+            )
+
