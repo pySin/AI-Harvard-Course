3737555f6d6b17cd06de371a2065b89c554ec329
pySin <sinan_km@yahoo.com>
2025-03-29 17:31:53 +0000

    Import libraries

---
+++ Learning/Supervised Learning/sl_student_pass.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
