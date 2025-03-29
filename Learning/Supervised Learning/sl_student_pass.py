00dfc3c1c4e00d5c1a87f5a347664389a33fc959
pySin <sinan_km@yahoo.com>
2025-03-29 17:34:22 +0000

    Add Supervised Learning features

--- Learning/Supervised Learning/sl_student_pass.py
+++ Learning/Supervised Learning/sl_student_pass.py
@@ -3,3 +3,6 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Features (hours studied)
+X = np.array([0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]).reshape(-1, 1)
