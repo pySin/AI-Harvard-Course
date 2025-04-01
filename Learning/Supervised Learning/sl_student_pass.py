4e5b38da087780a28ade5df763140f6fdd2ce1e1
pySin <sinan_km@yahoo.com>
2025-04-01 15:25:50 +0100

    Add test data

--- Learning/Supervised Learning/sl_student_pass.py
+++ Learning/Supervised Learning/sl_student_pass.py
@@ -6,3 +6,8 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 # Features (hours studied)
 X = np.array([0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]).reshape(-1, 1)
+
+# Labels (0 = fail, 1 = pass)
+y = np.array([0, 0, 0, 0, 1, 0, 1, 1, 1, 1])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
