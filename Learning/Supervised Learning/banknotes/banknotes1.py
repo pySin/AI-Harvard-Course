b669b4edcf60b568867f4799e070ae3cffe42f21
pySin <sinan_km@yahoo.com>
2025-04-04 16:30:14 +0100

    Potential models.

--- Learning/Supervised Learning/banknotes/banknotes1.py
+++ Learning/Supervised Learning/banknotes/banknotes1.py
@@ -6,3 +6,8 @@ from sklearn.linear_model import Perceptron
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
+
+model = Perceptron()
+# model = svm.SVC()
+# model = KNeighborsClassifier(n_neighbors=1)
+# model = GaussianNB()
