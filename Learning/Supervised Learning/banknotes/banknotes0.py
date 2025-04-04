d9e7a457ced77dbc8e3753e9dc32b408195d4302
pySin <sinan_km@yahoo.com>
2025-04-04 14:57:09 +0100

    Start banknotes task

---
+++ Learning/Supervised Learning/banknotes/banknotes0.py
@@ -0,0 +1,7 @@
+import csv
+import random
+
+from sklearn import svm
+from sklearn.linear_model import Perceptron
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
