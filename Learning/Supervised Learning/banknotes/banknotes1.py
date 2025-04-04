e84cee2c8a5efde15d1c0d82524e90e1567b88a7
pySin <sinan_km@yahoo.com>
2025-04-04 16:35:21 +0100

    Read CSV data.

--- Learning/Supervised Learning/banknotes/banknotes1.py
+++ Learning/Supervised Learning/banknotes/banknotes1.py
@@ -11,3 +11,15 @@ model = Perceptron()
 # model = svm.SVC()
 # model = KNeighborsClassifier(n_neighbors=1)
 # model = GaussianNB()
+
+# Read data in from file
+with open("banknotes.csv") as f:
+    reader = csv.reader(f)
+    next(reader)
+
+    data = []
+    for row in reader:
+        data.append({
+            "evidence": [float(cell) for cell in row[:4]],
+            "label": "Authentic" if row[4] == "0" else "Counterfeit"
+        })
