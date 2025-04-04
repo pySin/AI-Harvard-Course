772bdaad157ee0375e0ad9dfd7929bc5685f0022
pySin <sinan_km@yahoo.com>
2025-04-04 16:29:07 +0100

    Further scikit-learn improvements.

---
+++ Learning/Supervised Learning/banknotes/banknotes1.py
@@ -0,0 +1,8 @@
+import csv
+import random
+
+from sklearn import svm
+from sklearn.linear_model import Perceptron
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
