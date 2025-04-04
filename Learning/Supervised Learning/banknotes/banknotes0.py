4634ee5a6a26883e28a8deaf10dd3f12725c8ff5
pySin <sinan_km@yahoo.com>
2025-04-04 15:02:55 +0100

    Read data

--- Learning/Supervised Learning/banknotes/banknotes0.py
+++ Learning/Supervised Learning/banknotes/banknotes0.py
@@ -10,3 +10,15 @@ from sklearn.neighbors import KNeighborsClassifier
 # model = svm.SVC()
 # model = KNeighborsClassifier(n_neighbors=1)
 model = GaussianNB()
+
+# Read data in from file
+with open("banknotes.csv") as f:
+    reader = csv.reader(f)
+    next(reader)
+
+    data = []
+    for row in reader:
+        data.append({
+            "evidence": [float(cell) for cell in row[:4]],
+            "label": "Authentic" if row[4] == "0" else "Counterfeit"
+        })
