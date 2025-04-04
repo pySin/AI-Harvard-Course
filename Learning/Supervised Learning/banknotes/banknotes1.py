b2bf4de404683da2793526675830622d1ffd26a0
pySin <sinan_km@yahoo.com>
2025-04-04 16:43:36 +0100

    Train and test data.

--- Learning/Supervised Learning/banknotes/banknotes1.py
+++ Learning/Supervised Learning/banknotes/banknotes1.py
@@ -23,3 +23,11 @@ with open("banknotes.csv") as f:
             "evidence": [float(cell) for cell in row[:4]],
             "label": "Authentic" if row[4] == "0" else "Counterfeit"
         })
+
+# Separate data into training and testing groups
+evidence = [row["evidence"] for row in data]
+labels = [row["label"] for row in data]
+
+X_training, X_testing, y_training, y_testing = train_test_split(
+    evidence, labels, test_size=0.4
+)
