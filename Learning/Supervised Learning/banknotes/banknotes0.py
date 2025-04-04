9f3e35357363af87973b5eb65c5b26524a82df67
pySin <sinan_km@yahoo.com>
2025-04-04 14:58:22 +0100

    Add different supervised models

--- Learning/Supervised Learning/banknotes/banknotes0.py
+++ Learning/Supervised Learning/banknotes/banknotes0.py
@@ -5,3 +5,8 @@ from sklearn import svm
 from sklearn.linear_model import Perceptron
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
+
+# model = Perceptron()
+# model = svm.SVC()
+# model = KNeighborsClassifier(n_neighbors=1)
+model = GaussianNB()
