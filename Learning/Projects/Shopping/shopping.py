31e916b40d2ba37d52729648674ca8ca777995e0
pySin <sinan_km@yahoo.com>
2025-04-07 16:33:48 +0100

    Evidence and labels

--- Learning/Projects/Shopping/shopping.py
+++ Learning/Projects/Shopping/shopping.py
@@ -112,6 +112,8 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
+    model = KNeighborsClassifier(n_neighbors=1)
+    model.fit(evidence, labels)
     raise NotImplementedError
 
 
