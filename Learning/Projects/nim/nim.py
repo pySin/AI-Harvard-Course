83779985b32ebb2e69e4f108030de5387b56cfc7
pySin <sinan_km@yahoo.com>
2025-04-08 02:21:55 +0100

    Initial Pile

--- Learning/Projects/nim/nim.py
+++ Learning/Projects/nim/nim.py
@@ -1,3 +1,17 @@
 import math
 import random
 import time
+
+class Nim():
+
+    def __init__(self, initial=[1, 3, 5, 7]):
+        """
+        Initialize game board.
+        Each game board has
+            - `piles`: a list of how many elements remain in each pile
+            - `player`: 0 or 1 to indicate which player's turn
+            - `winner`: None, 0, or 1 to indicate who the winner is
+        """
+        self.piles = initial.copy()
+        self.player = 0
+        self.winner = None
