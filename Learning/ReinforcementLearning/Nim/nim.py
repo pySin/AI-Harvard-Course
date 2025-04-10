1fd07df13dc7a2e7e6a66184cf0a009a634ece3a
pySin <sinan_km@yahoo.com>
2025-04-10 18:33:45 +0100

    Choose action states check

--- Learning/ReinforcementLearning/Nim/nim.py
+++ Learning/ReinforcementLearning/Nim/nim.py
@@ -99,7 +99,10 @@ class NimAI():
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        raise NotImplementedError
+        if (state, action) not in self.q:
+            return 0
+        else:
+            return self.q[(state, action)]
 
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
--- Learning/Test Dir/trys.py
+++ Learning/Test Dir/trys.py
@@ -1,1 +1,6 @@
 # Try functionalities
+
+q = dict()
+q[(0, 0, 0, 2), (3, 2)] = -1
+
+print(q[((0, 0, 0, 2), (3, 2))])
